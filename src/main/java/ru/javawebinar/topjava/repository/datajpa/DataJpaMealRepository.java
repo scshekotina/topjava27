b250bdf01585586e64617c4afa54fb9fbc1f49b1
Anastasiya Scshekotina <lenavl@list.ru>
2022-11-19 12:29:02 +0700

    Fix bugs in DataJpaMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -11,44 +11,47 @@ import java.util.List;
 @Repository
 public class DataJpaMealRepository implements MealRepository {
 
-    private final CrudMealRepository crudRepository;
+    private final CrudMealRepository crudMealRepository;
+    private final CrudUserRepository crudUserRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
-        this.crudRepository = crudRepository;
+    public DataJpaMealRepository(CrudMealRepository crudMealRepository, CrudUserRepository crudUserRepository) {
+        this.crudMealRepository = crudMealRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Transactional
     @Override
     public Meal save(Meal meal, int userId) {
         if (!meal.isNew()) {
-            Meal found = crudRepository.findById(meal.id()).orElse(null);
+            Meal found = crudMealRepository.findById(meal.id()).orElse(null);
             if (found == null || found.getUser() == null || found.getUser().getId() == null ||
                     found.getUser().getId() != userId) {
                 return null;
             }
         }
-        return crudRepository.save(meal);
+        meal.setUser(crudUserRepository.getReferenceById(userId));
+        return crudMealRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return crudRepository.delete(id, userId) != 0;
+        return crudMealRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        Meal meal = crudRepository.findById(id).orElse(null);
-        return meal != null && meal.getUser() == null || meal.getUser().getId() == null ||
+        Meal meal = crudMealRepository.findById(id).orElse(null);
+        return meal != null && meal.getUser() != null && meal.getUser().getId() != null &&
                 meal.getUser().getId() == userId ? meal : null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return crudRepository.getAll(userId);
+        return crudMealRepository.getAll(userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return crudRepository.getBetweenHalfOpen(startDateTime, endDateTime, userId);
+        return crudMealRepository.getBetweenHalfOpen(startDateTime, endDateTime, userId);
     }
 }
